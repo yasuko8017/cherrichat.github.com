637b411b7ae2d4d747904d9f5802f810259864f2
yasuko8017 <yasuko8017@hotmail.com>
2020-01-28 18:57:52 +0800

    sass導入、加入reset.sass

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app">
-
+    <div class="sidebar">
+      好友列表
+    </div>
+    <div class="main">
+    </div>
   </div>
 </template>
 
@@ -8,6 +12,25 @@
 
 </script>
 
-<style>
-
+<style lang="sass" src="./style/reset.sass"></style>
+<style lang="scss">
+#app{
+  width: 900px;
+  height: 600px;
+  background: #000;
+  margin: 50px auto;
+  .sidebar, .main {
+    height: 100%;
+  }
+  .sidebar {
+    float: left;
+    width: 250px;
+    background: #4A90E2;
+  }
+  .main {
+    float: left;
+    width: 650px;
+    background: #4A9123;
+  }
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import './style/reset.sass';
+
 new Vue({
   el: '#app',
   render: h => h(App)
